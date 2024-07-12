1dff2ebc9ff6934c3b12566e93e3adcda7dc689c
john karson <nuttall@nuttall.nuttall>
2024-07-12 21:27:17 +0300

    e-comerce platform

--- backend/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createOrder, getOrders } = require('../controllers/orderController');
-const router = express.Router();
-
-router.route('/').post(createOrder).get(getOrders);
-
-module.exports = router;
--- backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getProducts, getProductById } = require('../controllers/productController');
-const router = express.Router();
-
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
-
-module.exports = router;
